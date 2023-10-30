ff3935e9ef47aa67db3175f53674008da6745f32
Stephen Vincent <stevevincent17@icloud.com>
2023-10-30 20:39:54 +0000

    added a menu and accepts user input, first bit of c++ code :)

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+int main()
+{
+    while (true)
+    {
+        // 1 print help
+        std::cout << "1. Print help " << std::endl;
+        // 2 print exchange stats
+        std::cout << "2. Print Exchange Stats " << std::endl;
+        // 3 make an offer
+        std::cout << "3. Make an offer " << std::endl;
+        // 4 make a bid
+        std::cout << "4. Make a bid " << std::endl;
+        // 5 print wallet
+        std::cout << "5. Print Wallet " << std::endl;
+        // 6 continue
+        std::cout << "6. Continue " << std::endl;
+
+        std::cout << "7. Exit" << std::endl;
+
+        std::cout << "=================" << std::endl;
+        std::cout << "Type in 1-6 " << std::endl;
+
+        int userOption;
+        std::cin >> userOption;
+        std::cout << "You chose: " << userOption << std::endl;
+
+        if (userOption == 0) // bad input
+        {
+            std::cout << "Invalid choice. Choose 1-6" << std::endl;
+        }
+        if (userOption == 1) // help
+        {
+            std::cout << "Help - your aim is to make money. Analyse the markets and make bids and offers." << std::endl;
+        }
+        if (userOption == 2) // Print Exchange Stats
+        {
+            std::cout << "Market looks good." << std::endl;
+        }
+        if (userOption == 3) // Make an offer.
+        {
+            std::cout << "Make an offer - Enter the amount" << std::endl;
+        }
+        if (userOption == 4) // Make a bid.
+        {
+            std::cout << "Make a bid - Enter the amount" << std::endl;
+        }
+        if (userOption == 5) // Print Wallet
+        {
+            std::cout << "Your wallet is empty" << std::endl;
+        }
+        if (userOption == 6) // Continue
+        {
+            std::cout << "Going to next timeframe" << std::endl;
+        }
+        if (userOption >= 7)
+        {
+            std::cout << "You exited the app." << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
